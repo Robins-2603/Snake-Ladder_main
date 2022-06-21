261a9fe00c951fae039aa59a71d1a65c31ebcf8a
Robins-2603 <robinsforwork@gmail.com>
2022-06-21 15:33:36 +0530

    Adding: player's initial position

--- src/com/snakeandladder/Game.java
+++ src/com/snakeandladder/Game.java
@@ -3,5 +3,7 @@ package com.snakeandladder;
 public class Game {
     public static void main(String[] args) {
         System.out.println("welcome to Snake & Ladder");
+        int player1 = 0;
+        System.out.println("position of player1 is " + player1);
     }
 }

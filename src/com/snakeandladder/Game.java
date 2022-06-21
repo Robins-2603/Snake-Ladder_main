3c47f42391c1408a099548b03a2c9c1065fbcd9e
Robins-2603 <robinsforwork@gmail.com>
2022-06-21 15:42:44 +0530

    Added: dice roll

--- src/com/snakeandladder/Game.java
+++ src/com/snakeandladder/Game.java
@@ -1,9 +1,15 @@
 package com.snakeandladder;
 
+import java.util.Random;
+
 public class Game {
     public static void main(String[] args) {
         System.out.println("welcome to Snake & Ladder");
         int player1 = 0;
-        System.out.println("position of player1 is " + player1);
+        System.out.println("initial position of player1 is " + player1);
+        // use Random for dice roll
+        Random dice = new Random();
+        player1 = dice.nextInt(6)+1;
+        System.out.println("play " + player1);
     }
 }

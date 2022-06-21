7c62bd0cfbad5bbcf101355f53858162c3be4e72
Robins-2603 <robinsforwork@gmail.com>
2022-06-21 19:49:51 +0530

    Adding: condition for no play, and array for ladder and snake

--- src/com/snakeandladder/Game.java
+++ src/com/snakeandladder/Game.java
@@ -3,13 +3,28 @@ package com.snakeandladder;
 import java.util.Random;
 
 public class Game {
+    public static final int Win_point = 100;
+    public static final int To_start = 1;
+
     public static void main(String[] args) {
         System.out.println("welcome to Snake & Ladder");
         int player1 = 0;
+        // creating Array for ladder
+        int[] ladder = {3,11,60,46,17};
+        // creating Array for snake
+        int[] snake = {99,70,52,25,95}
         System.out.println("initial position of player1 is " + player1);
         // use Random for dice roll
         Random dice = new Random();
         player1 = dice.nextInt(6)+1;
         System.out.println("play " + player1);
+        //condition for no play, ladder and snake
+        switch (player1){
+            case To_start:
+                System.out.println("Start your game play "+player1);
+                break;
+            default:
+                System.out.println("oops! you need 1 to start your game"); //no play
+        }
     }
 }
